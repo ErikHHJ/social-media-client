c9b1a085e3ffd5e0c902b181f8cd609a889b3bf9
ErikHHJ <erikhhju@gmail.com>
2023-11-12 00:25:34 +0100

    Made eslint, prettier autoperform on save and commit

--- src/js/api/apiBase.js
+++ src/js/api/apiBase.js
@@ -1,2 +1,3 @@
-export const apiBase = "https://whatver.com/api/posts";
+export const apiBase = 'https://whatver.com/api/posts';
 export const url = new URL(apiBase);
+console.log('Intentional Syntax Error');
